a0131b8a91e2caf8411e64edf40ae09dd37eefc7
Leslie <chukwunweikel@accessbankplc.com>
2025-04-21 23:44:56 +0100

    I added the turso config for the remote db

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,5 +1,15 @@
-import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
-import Database from 'better-sqlite3';
+// import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
+// import Database from 'better-sqlite3';
  
-export const sqlite = new Database('sqlite.db');
-export const db: BetterSQLite3Database = drizzle(sqlite);
+// export const sqlite = new Database('sqlite.db');
+// export const db: BetterSQLite3Database = drizzle(sqlite);
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+
+const client = createClient({
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
+export const db = drizzle(client, {
+//   logger: true,
+});
